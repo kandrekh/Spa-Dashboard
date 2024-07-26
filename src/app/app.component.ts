0559552cbcad583ecec16c1c35a0301dd541d38a
K-Andre Harris <123778827+kdreh@users.noreply.github.com>
2024-07-26 19:33:56 -0400

    Add authentication, layout, and shared components
    Implemented sign-in, sign-up, reset-password, and email-confirmation pages under authentication. Added navigation and sidebar support in the layout. Introduced shared components like loading, alerts, cards, charts, splash-screen, and page-not-found. Updated routes and styles to incorporate these changes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavigationComponent} from "./layout/navigation/navigation.component";
 import {NgStyle} from "@angular/common";
+import {AuthService} from "./authentication/services/auth.service";
+import {SignInComponent} from "./authentication/pages/sign-in/sign-in.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-    imports: [RouterOutlet, NavigationComponent, NgStyle],
+  imports: [RouterOutlet, NavigationComponent, NgStyle, SignInComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'pages';
+
+  authService = inject(AuthService);
+
 }
--- src/app/authentication/services/auth.service.ts
+++ src/app/authentication/services/auth.service.ts
@@ -5,5 +5,6 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
 
+  isLoggedIn: boolean = true;
   constructor() { }
 }
